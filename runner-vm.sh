cfa6f02089328e3b05540e14d388948ed1c7f7e0
Emil <emil@appunite.com>
2018-02-07 21:48:01 +0100

    add dedicated script for shell runner

---
+++ runner-vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "> Updating date:"
+sudo ntpdate -u time.apple.com && date
+
+echo -e "> Updating CA cert..."
+curl -kfsSL curl.haxx.se/ca/cacert.pem -o "$(ruby -ropenssl -e 'puts OpenSSL::X509::DEFAULT_CERT_FILE')"
+
+echo -e "> CPU:"
+sysctl -n machdep.cpu.brand_string
+
+echo -e "> Swift version:"
+xcrun swift -version
+
+echo -e "> Xcodebuild version:"
+xcodebuild -version
