65fdb5f70374ee76d53b9d688035f18a925234a5
Emil <emil.wojtaszek@gmail.com>
2018-02-07 15:29:19 +0100

    Update gitlab-runner-pre-script.sh

--- gitlab-runner-pre-script.sh
+++ gitlab-runner-pre-script.sh
@@ -4,7 +4,7 @@ echo -e "> Updating date:"
 sudo ntpdate -u time.apple.com
 
 echo -e "> Updating CA cert..."
-curl -fsSL curl.haxx.se/ca/cacert.pem -o "$(ruby -ropenssl -e 'puts OpenSSL::X509::DEFAULT_CERT_FILE')"
+curl -kfsSL curl.haxx.se/ca/cacert.pem -o "$(ruby -ropenssl -e 'puts OpenSSL::X509::DEFAULT_CERT_FILE')"
 
 echo -e "> CPU:"
 sysctl -n machdep.cpu.brand_string
