f99f3259193bed6036b3ab7139151959faa63999
Emil <emil@appunite.com>
2018-02-21 13:41:23 +0100

    cleanup

--- runner-shell.sh
+++ runner-shell.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo -e "> Updating CA cert..."
+echo -e "--- Updating CA cert..."
 curl -kfsSL curl.haxx.se/ca/cacert.pem -o "$(ruby -ropenssl -e 'puts OpenSSL::X509::DEFAULT_CERT_FILE')"
 
-echo -e "> CPU:"
+echo -e "--- CPU:"
 sysctl -n machdep.cpu.brand_string
 
-echo -e "> Swift version:"
+echo -e "--- Swift version:"
 xcrun swift -version
 
-echo -e "> Xcodebuild version:"
+echo -e "--- Xcodebuild version:"
 xcodebuild -version
